7550619865df467d0189b2cfd6d78fad319c9707
Alvin Nguyen <alvin.nguyen@forevernew.com.au>
2016-11-29 10:40:29 +1100

    Create Transaction Model

---
+++ database/migrations/2016_11_28_233552_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    const TABLE_NAME = 'transactions';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('category');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop(self::TABLE_NAME);
+    }
+}
